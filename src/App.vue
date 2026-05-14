4dd5dc62f968c3cf53efd3999924442e14174fd6
Félix Ouvry <119581606+FlixBusKawaii@users.noreply.github.com>
2026-05-14 22:43:16 +0200

    feature : app layout (tailwindcss), bottom nav bar, new icons (+ cleanup)

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,24 @@
 <script setup lang="ts">
+import BottomNavigation from './components/BottomNavigation.vue';
+import AppIcon from './components/icons/AppIcon.vue';
+
 </script>
 
 <template>
-  <router-view />
+  <div class="min-h-screen bg-slate-950 text-white">
+    <div class="mx-auto flex min-h-screen max-w-lg flex-col">
+      <header class="border-b border-slate-800 px-4 py-3">
+        <div class="flex items-center gap-3 ">
+          <app-icon class="h-8 w-8 shrink-0"/>
+          <h1 class="tracking-wide">ECAS</h1>
+        </div>
+      </header>
+
+      <main class="flex-1 overflow-auto">
+        <RouterView />
+      </main>
+
+      <bottom-navigation />
+    </div>
+  </div>
 </template>
---
+++ src/components/BottomNavigation.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import HomeIcon from './icons/HomeIcon.vue';
+import ReadingsIcon from './icons/ReadingsIcon.vue';
+import StatsIcon from './icons/StatsIcon.vue';
+</script>
+
+<template>
+  <nav class="border-t border-slate-800 bg-slate-900">
+    <ul class="grid grid-cols-3">
+      <li>
+        <RouterLink
+          to="/home"
+          class="flex flex-col items-center gap-1 px-2 py-3 text-xs border-r border-r-white"
+        >
+          <HomeIcon class="h-6 w-6 text-white" />
+          <span>Home</span>
+        </RouterLink>
+      </li>
+
+      <li>
+        <RouterLink
+          to="/reads"
+          class="flex flex-col items-center gap-1 px-2 py-3 text-xs border-r border-r-white"
+        >
+          <ReadingsIcon class="h-6 w-6" />
+          <span>Readings</span>
+        </RouterLink>
+      </li>
+
+      <li>
+        <RouterLink
+          to="/stats"
+          class="flex flex-col items-center gap-1 px-2 py-3 text-xs"
+        >
+          <StatsIcon class="h-6 w-6" />
+          <span>Stats</span>
+        </RouterLink>
+      </li>
+    </ul>
+  </nav>
+</template>
